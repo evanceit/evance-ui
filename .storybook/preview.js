1a6354d2ae6a0f61219c20292d4c1c9de43498c0
Geoff Willings <geoff@azexis.com>
2023-06-06 21:12:21 +0100

    Attempt to fix dark mode in storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,32 @@
-import { themes } from '@storybook/theming';
+import { create, themes } from '@storybook/theming';
+import React from 'react';
+import { DocsContainer } from '@storybook/addon-docs';
+import { useDarkMode } from 'storybook-dark-mode'
 
 import '../src/css/reset.scss';
+
+/**
+ * Light Mode Theme
+ * ---
+ */
+const lightTheme = create({
+  base: 'light',
+  appBg: '#F3F5F9',
+  appContentBg: '#F3F5F9',
+  barBg: '#F3F5F9'
+});
+
+/**
+ * Dark Mode Theme
+ * ---
+ */
+const darkTheme = create({
+  base: 'dark',
+  appBg: '#1E1D2B',
+  appContentBg: '#1E1D2B',
+  barBg: '#1E1D2B'
+});
+
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
@@ -13,9 +39,9 @@ const preview = {
       },
     },
     // Override the default dark theme
-    dark: { ...themes.dark },
+    dark: darkTheme,
     // Override the default light theme
-    light: { ...themes.normal },
+    light: lightTheme,
     // Set Dark Mode Parameters
     darkMode: {
       classTarget: 'html',
@@ -23,7 +49,15 @@ const preview = {
       darkClass: 'ev-theme-dark',
       lightClass: 'ev-theme-light',
       stylePreview: true
-    }
+    },
+    docs: {
+      container: (props) => {
+        const isDark = useDarkMode();
+        const currentProps = { ...props };
+        currentProps.theme = isDark ? darkTheme : lightTheme;
+        return React.createElement(DocsContainer, currentProps);
+      },
+    },
   },
 };
 
