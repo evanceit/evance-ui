d16ef482e009093462a0e89b653a0378d397f7c0
Geoff Willings <geoff@azexis.com>
2023-07-01 16:26:17 +0100

    Add Editor list icons

--- src/icons/editor/index.ts
+++ src/icons/editor/index.ts
@@ -2,6 +2,8 @@ export { default as Bold } from './glyph/Bold.svg';
 export { default as ClearFormatting } from './glyph/ClearFormatting.svg';
 export { default as Italics } from './glyph/Italics.svg';
 export { default as ItalicsOff } from './glyph/ItalicsOff.svg';
+export { default as ListBullet } from './glyph/ListBullet.svg';
+export { default as ListNumbered } from './glyph/ListNumbered.svg';
 export { default as Overline } from './glyph/Overline.svg';
 export { default as Strikethrough } from './glyph/Strikethrough.svg';
 export { default as Underline } from './glyph/Underline.svg';
