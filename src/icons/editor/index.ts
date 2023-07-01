62d3f52e44524debab41f1f7022a307b8bf4a8e4
Geoff Willings <geoff@azexis.com>
2023-07-01 16:31:07 +0100

    Add editor dimension icons

--- src/icons/editor/index.ts
+++ src/icons/editor/index.ts
@@ -1,5 +1,8 @@
 export { default as Bold } from './glyph/Bold.svg';
 export { default as ClearFormatting } from './glyph/ClearFormatting.svg';
+export { default as DimensionAngle } from './glyph/DimensionAngle.svg';
+export { default as DimensionHeight } from './glyph/DimensionHeight.svg';
+export { default as DimensionWidth } from './glyph/DimensionWidth.svg';
 export { default as Italics } from './glyph/Italics.svg';
 export { default as ItalicsOff } from './glyph/ItalicsOff.svg';
 export { default as ListBullet } from './glyph/ListBullet.svg';
