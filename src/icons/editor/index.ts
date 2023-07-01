c3a4000780a3aadd1b4d02691dad1eb10f567484
Geoff Willings <geoff@azexis.com>
2023-07-01 16:20:44 +0100

    Add Editor Formatting Icons

---
+++ src/icons/editor/index.ts
@@ -0,0 +1,7 @@
+export { default as Bold } from './glyph/Bold.svg';
+export { default as ClearFormatting } from './glyph/ClearFormatting.svg';
+export { default as Italics } from './glyph/Italics.svg';
+export { default as ItalicsOff } from './glyph/ItalicsOff.svg';
+export { default as Overline } from './glyph/Overline.svg';
+export { default as Strikethrough } from './glyph/Strikethrough.svg';
+export { default as Underline } from './glyph/Underline.svg';
