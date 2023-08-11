6cf82f9f1cba963bec87453036316eb0a279d19d
Geoff Willings <geoff@azexis.com>
2023-08-11 18:03:12 +0100

    Remove import method from LanguagePack, we're not using it and Vite doesn't like it.

--- src/modules/Translation/LanguagePack.ts
+++ src/modules/Translation/LanguagePack.ts
@@ -1,5 +1,4 @@
-import * as fs from "fs";
-import {getPropertyValueByPath, isString} from "@/util";
+import {getPropertyValueByPath} from "@/util";
 import {Translatable} from "@/modules/Translation/Translatable.ts";
 
 /**
@@ -25,26 +24,6 @@ export class LanguagePack {
             return null;
         }
         return new Translatable(this.translationCode, reference, translationData);
-    }
-
-    /**
-     * # Import
-     *
-     * Load a language pack from a JavaScript or TypeScript source file.
-     *
-     * @param filePath
-     */
-    public async import(filePath: string) {
-        if (!fs.existsSync(filePath)) {
-            return false;
-        }
-        try {
-            const module = await import(filePath);
-            this.data = module.default || {};
-            return true;
-        } catch (error) {
-            return false;
-        }
     }
 }
 
