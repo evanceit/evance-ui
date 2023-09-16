47d9329fb2fdf6e8ffcb6832a5ac48728e62b200
Geoff Willings <geoff@azexis.com>
2023-09-16 15:59:43 +0100

    Implement FormField into EvSelect

--- src/components/EvSelect/EvSelect.stories.ts
+++ src/components/EvSelect/EvSelect.stories.ts
@@ -8,14 +8,46 @@ const meta: Meta<typeof EvSelect> = {
     component: EvSelect,
     title: 'Forms/EvSelect',
     argTypes: {
+        // Form Field Attributes
+        disabled: {
+            control: 'boolean',
+            description: 'Removes the ability to click or target the input.'
+        },
+        error: {
+            control: 'boolean',
+            description: 'Puts the input in a manual error state.'
+        },
+        id: {
+            control: 'text',
+            description: 'The ID associated with the form field and the component wrapper'
+        },
+        modelValue: {
+            description: "The `model-value` is the `v-model` value of the component."
+        },
+        name: {
+            control: 'text',
+            description: "Sets the name attribute of the internal input."
+        },
+        readonly: {
+            control: 'boolean'
+        },
+        'validate-on': {
+            type: 'select',
+            description: 'Change what type of event triggers validation to run.',
+            options: ['lazy', 'blur', 'input', 'submit', 'blur lazy', 'input lazy', 'submit lazy']
+        },
+        validators: {
+            description: 'An array of validator functions, which evaluate an input value ' +
+                'as an argument and must return either true or a string containing an error message. ' +
+                'The input field will enter an error state if a function returns'
+        },
+        // End
+
         appearance: {
             control: 'select',
             options: Object.values(InputAppearance)
         },
         clearable: {
-            control: 'boolean'
-        },
-        disabled: {
             control: 'boolean'
         },
         'icon-start': {
@@ -58,9 +90,19 @@ const meta: Meta<typeof EvSelect> = {
         },
     },
     args: {
+        // Form Field Args
+        disabled: false,
+        error: false,
+        id: undefined,
+        modelValue: undefined,
+        name: undefined,
+        readonly: false,
+        'validate-on': 'input',
+        validators: undefined,
+        // End
+
         appearance: InputAppearance.default,
         clearable: false,
-        disabled: false,
         'icon-start': 'none',
         multiple: false,
         'open-on-clear': false,
