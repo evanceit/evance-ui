94caf419aea13228f0de85efa8a56ae33ccc96a6
Geoff Willings <geoff@azexis.com>
2023-11-27 11:52:50 +0000

    Addition of spacer (the beginnings of Grid system)

---
+++ src/components/EvGrid/EvSpacer.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import './EvGrid.scss';
+</script>
+<template>
+    <div class="ev-spacer"></div>
+</template>
---
+++ src/components/EvGrid/index.ts
@@ -0,0 +1,2 @@
+
+export { default as EvSpacer } from './EvSpacer.vue';
