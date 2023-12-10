869c6389921b12ef15051a081d36d526d0af177c
Geoff Willings <geoff@azexis.com>
2023-12-10 12:12:08 +0000

    Kind of figured out how to programmatically render dialogs.

--- src/services/EvDialogService.ts
+++ src/services/EvDialogService.ts
@@ -1,16 +1,63 @@
-import {App} from "vue";
+import {App, h, markRaw, mergeProps, nextTick, render, shallowRef, toRaw, watch} from "vue";
 import {EvDialogSymbol} from "@/composables/dialog.ts";
+import {EvDialog} from "@/components";
 
 /**
  * EvDialogService
+ *
+ * @todo: we need some way for the service to mount itself
+ *
+ * I have seen this done before int a basic confirm dialog plugin
+ * we need to do something similar but in a more advanced way.
+ * @see https://github.com/Godofbrowser/vuejs-dialog/blob/master/src/plugin/js/index.js
  */
 export const EvDialogService = {
     install: (app: App) => {
+
+
         const service = {
             open: (options) => {
+                let instance;
+                let node = document.createElement('div');
+                node.id = 'test-1';
+                document.querySelector('body')!.appendChild(node);
 
+                const modelValue = shallowRef(false);
+                const props = options.props ?? {};
+                const internalSlots = options.slots ?? {};
+                const renderedSlots = {};
 
-                console.log('open dialog', options);
+                // Build slots
+                for (const key in internalSlots) {
+                    const component = internalSlots[key];
+                    renderedSlots[key] = (props, slots) => {
+                        return h(component, props, slots);
+                    };
+                }
+
+                function onAfterLeave() {
+                    const el = instance.$el.parentNode;
+                    render(null, el);
+                    el.parentNode.removeChild(el);
+                }
+
+                const renderedDialog = h(
+                    EvDialog,
+                    mergeProps(
+                        props,
+                        {
+                            modelValue,
+                            onAfterLeave
+                        },
+                    ),
+                    renderedSlots
+                );
+                renderedDialog.appContext = app._context;
+                render(renderedDialog, node);
+                instance = renderedDialog.component.proxy;
+                nextTick(() => {
+                    modelValue.value = true;
+                });
             }
         };
 
--- src/stories/EvDialogService/ExampleDialog.vue
+++ src/stories/EvDialogService/ExampleDialog.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import {useDialog} from "@/composables/dialog.ts";
+import {EvButton} from "@/components";
+import ExampleDialog from "@/stories/EvDialogService/ExampleDialog.vue";
 
+const dialog = useDialog();
+
+function open() {
+    dialog.open({
+        slots: {
+            default: ExampleDialog
+        }
+    });
+}
 </script>
 <template>
     <h3>
@@ -8,4 +20,5 @@
     <p>
         This is an example dialog component.
     </p>
+    <ev-button @click="open">Open another</ev-button>
 </template>
