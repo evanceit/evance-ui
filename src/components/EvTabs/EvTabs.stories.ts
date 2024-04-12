acc8943e1746acc45cdc9df14ed32b98b091e3a2
Geoff Willings <geoff@azexis.com>
2024-04-12 22:09:37 +0100

    Update EvTabs stories

--- src/components/EvTabs/EvTabs.stories.ts
+++ src/components/EvTabs/EvTabs.stories.ts
@@ -3,6 +3,7 @@ import type {Meta, StoryObj} from "@storybook/vue3";
 import {EvTab, EvTabs} from "../EvTabs";
 import {SalesEnquiry, SalesEnquiryFill, SalesOrder, SalesOrderFill, SalesQuotation, SalesQuotationFill} from "@/icons";
 import {shallowRef} from "vue";
+import {EvIcon} from "@/components";
 
 const meta: Meta<typeof EvTabs> = {
     component: EvTabs,
@@ -40,7 +41,7 @@ type Story = StoryObj<typeof EvTabs>;
 
 export const Primary: Story = {
     render: (args: any) =>  ({
-        components: { EvTabs, EvTab },
+        components: { EvTabs, EvTab, EvIcon },
         setup() {
             const selection = shallowRef(1);
 
@@ -56,6 +57,7 @@ export const Primary: Story = {
             };
         },
         template: `
+            <p>Example of tags with labels and icons</p>
             <ev-tabs v-bind="args" v-model="selection">
                 <ev-tab 
                     text="Orders" 
@@ -69,6 +71,20 @@ export const Primary: Story = {
                     text="Quotes" 
                     :value="3" 
                     :icon-start="selection == 3 ? SalesQuotationFill : SalesQuotation"  />
+            </ev-tabs>
+            
+            <p>&nbsp;</p>
+            <p>Example of tags with icon only</p>
+            <ev-tabs v-bind="args" v-model="selection">
+                <ev-tab :value="1" :icon="true">
+                    <ev-icon :glyph="selection == 1 ? SalesOrderFill : SalesOrder" />
+                </ev-tab>
+                <ev-tab :value="2" :icon="true">
+                    <ev-icon :glyph="selection == 2 ? SalesEnquiryFill : SalesEnquiry" />
+                </ev-tab>
+                <ev-tab :value="3" :icon="true">
+                    <ev-icon :glyph="selection == 3 ? SalesQuotationFill : SalesQuotation" />
+                </ev-tab>
             </ev-tabs>
         `
     })
