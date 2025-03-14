0e479a9da5aa56c85756d7d97a08e4a35d7e6d46
Geoff Willings <geoff@azexis.com>
2025-03-14 23:24:00 +0000

    Add default body scope to theme mode in `<ev-app>`

--- src/components/EvApp/EvApp.ts
+++ src/components/EvApp/EvApp.ts
@@ -1,9 +1,16 @@
 import { propsFactory } from "@/util";
 import { makeComponentProps } from "@/composables";
 import { makeThemeProps } from "@/composables/theme";
+import { PropType } from "vue";
+
+export type ThemeScope = "body" | "app";
 
 export const makeEvAppProps = propsFactory(
     {
+        themeScope: {
+            type: String as PropType<ThemeScope>,
+            default: "body",
+        },
         ...makeComponentProps(),
         ...makeThemeProps(),
     },
--- src/components/EvApp/EvApp.vue
+++ src/components/EvApp/EvApp.vue
@@ -2,14 +2,37 @@
 import "./EvApp.scss";
 import { makeEvAppProps } from "./EvApp";
 import { provideTheme } from "@/composables/theme";
+import { computed, onMounted, watch } from "vue";
 
 const props = defineProps({ ...makeEvAppProps() });
 const { themeClasses } = provideTheme(props);
 
+const appThemeClasses = computed(() => {
+    return props.themeScope === "app" ? themeClasses.value : undefined;
+});
+
+const bodyThemeClasses = computed(() => {
+    return props.themeScope === "body" ? themeClasses.value : undefined;
+});
+
+watch(bodyThemeClasses, (newValue, oldValue) => {
+    if (oldValue) {
+        document.body.classList.remove(oldValue);
+    }
+    if (newValue) {
+        document.body.classList.add(newValue);
+    }
+});
+
+onMounted(() => {
+    if (bodyThemeClasses.value) {
+        document.body.classList.add(bodyThemeClasses.value);
+    }
+});
 </script>
 
 <template>
-    <div :class="['ev-application', themeClasses, props.class]">
+    <div :class="['ev-application', appThemeClasses, props.class]">
         <div class="ev-application--container">
             <slot />
         </div>
