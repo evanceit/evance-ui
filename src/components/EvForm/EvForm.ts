6c19ca02848fd3f02312180fb198724dbbbbe77c
Geoff Willings <geoff@azexis.com>
2025-06-04 22:52:35 +0100

    Refactor composables exported

--- src/components/EvForm/EvForm.ts
+++ src/components/EvForm/EvForm.ts
@@ -1,6 +1,8 @@
 import { propsFactory } from "@/util";
-import { makeComponentProps } from "@/composables/component";
-import { makeFormProps } from "@/composables/form";
+import { ComponentProps, makeComponentProps } from "@/composables/component";
+import { FormProps, makeFormProps } from "@/composables/form";
+
+export interface EvFormProps extends FormProps, ComponentProps {}
 
 export const makeEvFormProps = propsFactory(
     {
--- src/composables/index.ts
+++ src/composables/index.ts
@@ -2,27 +2,29 @@
  * PUBLIC INTERFACES ONLY
  * Imports in our code should be to the composable directly, not this file
  */
-export { useDate } from "./date";
-export { useDefaults } from "./defaults";
-export { useDisplay } from "./display";
-export { useLocaleManager, useLocaleFunctions } from "./locale";
-
-export type { DateAdapterInstance } from "./date";
-export type { DefaultsInstance } from "./defaults";
-export type {
-    DisplayBreakpoint,
-    DisplayInstance,
-    DisplayThresholds,
-} from "./display";
-export type { IconValue } from "./icons";
-
-// Components
-export { IconProp } from "./icons";
-export { makeComponentProps } from "./component";
-export { makeDimensionsProps } from "./dimensions";
-export { makeTagProps } from "./tag";
-
-// Service related composables
+export { makeComponentProps, type ComponentProps } from "./component";
+export { useDate, type DateAdapterInstance } from "./date";
+export { useDefaults, type DefaultsInstance } from "./defaults";
 export { useDialog, injectDialog } from "./dialog";
+export {
+    makeDimensionsProps,
+    useDimensions,
+    type DimensionsProps,
+} from "./dimensions";
+export {
+    calculateDisplayRuleValue,
+    useDisplay,
+    useBreakpointClasses,
+    useDisplayRuleClasses,
+    type DisplayBreakpoint,
+    type DisplayInstance,
+    type DisplayThresholds,
+} from "./display";
 export { useDrawer, injectDrawer } from "./drawer";
+export type { SubmitEventPromise, FormValidationResult } from "./form";
+export { IconProp, type IconValue } from "./icons";
+export { useLocaleManager, useLocaleFunctions } from "./locale";
+export { useModelProxy } from "./modelProxy";
 export { useNotification } from "./notification";
+export { makeTagProps } from "./tag";
+export { useTheme } from "./theme";
