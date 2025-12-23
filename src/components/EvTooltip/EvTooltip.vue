8c51c228566aa08069da8fa53ac32bb4ec5cff17
Geoff Willings <geoff@azexis.com>
2025-12-23 16:56:02 +0000

    Fix `EvTooltip` activator slot props definition.

--- src/components/EvTooltip/EvTooltip.vue
+++ src/components/EvTooltip/EvTooltip.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 });
 
 const slots = defineSlots<{
-    activator(): never;
+    activator(props: { isActive: boolean; props: any }): never;
     default(): never;
 }>();
 
