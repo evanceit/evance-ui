2614d7ecc842e7e63e6ef33f93a81f45d78cdd53
Geoff Willings <geoff@azexis.com>
2026-02-22 17:45:24 +0000

    Add `variant` prop to `EvExpansionPanels` and update styles for accordion support

--- src/components/EvExpansionPanel/EvExpansionPanels/EvExpansionPanels.ts
+++ src/components/EvExpansionPanel/EvExpansionPanels/EvExpansionPanels.ts
@@ -1,9 +1,20 @@
 import { propsFactory } from "@/util";
 import { makeComponentProps, makeTagProps } from "@/composables";
 import { makeGroupProps } from "@/composables/group";
+import { PropType } from "vue";
+
+const allowedVariants = ["default", "accordion"] as const;
+
+type ExpansionPanelsVariant = (typeof allowedVariants)[number];
 
 export const makeEvExpansionPanelsProps = propsFactory(
     {
+        variant: {
+            type: String as PropType<ExpansionPanelsVariant>,
+            default: "default",
+            validator: (v: string) =>
+                allowedVariants.includes(v as ExpansionPanelsVariant),
+        },
         ...makeGroupProps(),
         ...makeComponentProps(),
         ...makeTagProps(),
