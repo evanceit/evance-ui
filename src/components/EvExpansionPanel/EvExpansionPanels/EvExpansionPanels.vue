36674634ffcdbf6de414385a793dddc9af4ddb03
Geoff Willings <geoff@azexis.com>
2026-02-22 17:56:56 +0000

    Fix variant styling for `ev-expansion-panels`

--- src/components/EvExpansionPanel/EvExpansionPanels/EvExpansionPanels.vue
+++ src/components/EvExpansionPanel/EvExpansionPanels/EvExpansionPanels.vue
@@ -3,6 +3,7 @@ import "./EvExpansionPanels.scss"
 import { makeEvExpansionPanelsProps } from "./EvExpansionPanels";
 import { useGroup } from "@/composables/group";
 import { EvExpansionPanelSymbol } from "@/components/EvExpansionPanel";
+import {computed} from "vue";
 
 const props = defineProps({
     ...makeEvExpansionPanelsProps()
@@ -12,18 +13,20 @@ const slots = defineSlots<{
     default(): never;
 }>();
 
-const { next, previous } = useGroup(props, EvExpansionPanelSymbol)
+const { next, previous } = useGroup(props, EvExpansionPanelSymbol);
 
 defineExpose({
     next,
     previous,
 });
 
+const variantClass = computed(() => `is-variant-${props.variant}`);
 </script>
 
 <template>
-    <component :is="props.tag"
-               :class="['ev-expansion-panels', props.class]">
+    <component
+        :is="props.tag"
+        :class="['ev-expansion-panels', variantClass, props.class]">
         <slot name="default" v-bind="{ next, previous }" />
     </component>
 </template>
